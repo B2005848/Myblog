1f9b2fc6ba12adc4e1a0d9a309868d086a3d1f4a
B2005848 <namltgt2002@gmail.com>
2025-02-17 22:51:11 +0700

    update

--- src/pages/introduction/index.vue
+++ src/pages/introduction/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h4 class="text-white text-4xl xl:text-7xl z-10 w-5/6">
       "Unlock your true potential by embracing challenges and pushing beyond
-      your limits."
+      your limits." 1 1
     </h4>
   </div>
 </template>
